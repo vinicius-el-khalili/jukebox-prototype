dccef7b6f7ff2f4b2c75f77070bfc11762f31329
vinicius-el-khalili <velkhaliliborsato@gmail.com>
2023-03-22 18:08:47 -0300

    (+) myPlaylist

--- pages/index.js
+++ pages/index.js
@@ -5,6 +5,15 @@ import AppContext from '@/context/AppContext'
 
 export default function Home() {
   const context = useContext(AppContext)
+  var myPlaylist = []
+  Object.keys(context.state.songCardFlag).map((key,index)=>{
+    console.log(key,context.state.songCardFlag[key],parseInt(key))
+    if (context.state.songCardFlag[key]){
+      console.log(context.songs[key])
+      myPlaylist = [...myPlaylist, ...[context.songs[key]]]
+      console.log(myPlaylist)
+    }
+  })
 
   return (
     <div className={style.container}>
@@ -18,20 +27,17 @@ export default function Home() {
         </div>
       </div>
 
-      { 
-        Object.keys(context.state.songCardFlag).map((_id,index)=>{
-          if (context.state.songCardFlag[_id]){
-            let song = context.songs.filter(s=>{return context.songs[parseInt(_id)]._id ===_id})
-            console.log("hmm")
-            return(
-              <div key={"kwp"+_id}>
-                Hey
-              </div>
-            )
-          }
-        }) 
-      
+      <div className={style.myPlaylist} >
+      {
+        myPlaylist.map( song => (
+          <div className={style.songText} key={"myplaylis_"+song._id}>
+            <p>{song.title}</p>
+            <div className={style.dot}/>
+            <p>{song.artist}</p>
+          </div>
+        ))
       }
+      </div>
 
     </div>
   )
